a76bc08a54f3ecc2837600ffe034b59945910bc9
daeks <daeks@gmx.de>
2017-02-21 21:22:03 +0100

    layout stuff

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
   
   $storage = system::getStorage();
   $sto_percent = round($storage['used']/$storage['total']*100, 2);
-  echo '<p>Used: <b>'.round($storage['used']/1024/1024, 2).'GB</b> ('.$sto_percent.'%) Free: <b>'.round($storage['free']/1024/1024, 2).'</b>GB Total: <b>'.round($storage['total']/1024/1024, 2).'GB</b></p>';
+  echo '<p>Used: <b>'.round($storage['used']/1024/1024, 2).'GB</b> ('.$sto_percent.'%) Free: <b>'.round($storage['free']/1024/1024, 2).'GB</b> Total: <b>'.round($storage['total']/1024/1024, 2).'GB</b></p>';
   echo '<div class="progress">';
   echo '<div class="progress-bar" role="progressbar" aria-valuenow="'.$storage['used'].'" aria-valuemin="0" aria-valuemax="'.$storage['total'].'" style="width: '.$sto_percent.'%;">'.$sto_percent.'%</div>';
   echo '</div>';
@@ -75,7 +75,7 @@
   
   $memory = system::getMemory();
   $mem_percent = round($memory['used']/$memory['total']*100, 2);
-  echo '<p>Used: <b>'.round($memory['used']/1024, 2).'MB</b> ('.$mem_percent.'%) Free: <b>'.round($memory['free']/1024, 2).'</b>MB Total: <b>'.round($memory['total']/1024, 2).'MB</b></p>';
+  echo '<p>Used: <b>'.round($memory['used']/1024, 2).'MB</b> ('.$mem_percent.'%) Free: <b>'.round($memory['free']/1024, 2).'MB</b> Total: <b>'.round($memory['total']/1024, 2).'MB</b></p>';
   echo '<div class="progress">';
   echo '<div class="progress-bar" role="progressbar" aria-valuenow="'.$memory['used'].'" aria-valuemin="0" aria-valuemax="'.$memory['total'].'" style="width: '.$mem_percent.'%;">'.$mem_percent.'%</div>';
   echo '</div>';
@@ -89,7 +89,7 @@
   
   $swap = system::getSwap();
   $swap_percent = round($swap['used']/$swap['total']*100, 2);
-  echo '<p>Used: <b>'.round($swap['used']/1024, 2).'MB</b> ('.$swap_percent.'%) Free: <b>'.round($swap['free']/1024, 2).'</b>MB Total: <b>'.round($swap['total']/1024, 2).'MB</b></p>';
+  echo '<p>Used: <b>'.round($swap['used']/1024, 2).'MB</b> ('.$swap_percent.'%) Free: <b>'.round($swap['free']/1024, 2).'MB</b> Total: <b>'.round($swap['total']/1024, 2).'MB</b></p>';
   echo '<div class="progress">';
   echo '<div class="progress-bar" role="progressbar" aria-valuenow="'.$swap['used'].'" aria-valuemin="0" aria-valuemax="'.$swap['total'].'" style="width: '.$swap_percent.'%;">'.$swap_percent.'%</div>';
   echo '</div>';
