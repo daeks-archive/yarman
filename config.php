d648504a7c66cce9a66028a29e5622ad12973a6e
daeks <daeks@users.noreply.github.com>
2017-02-23 11:40:49 +0100

    travis test

--- config.php
+++ config.php
@@ -1,26 +1,26 @@
 <?php
 
-  define('BASE', (($_SERVER['DOCUMENT_ROOT'] != '') ? $_SERVER['DOCUMENT_ROOT'] : dirname(realpath(__FILE__))));
-  define('NAME', 'RetroPie');
-  define('BRAND', 'core/img/site-logo.png');
-  define('COOKIE_LIFETIME', 60*60*24*7*4*3);
-  
-  define('INC', BASE.DIRECTORY_SEPARATOR.'core');
-  define('JS', BASE.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'js');
-  define('CSS', BASE.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'css');
-  define('DATA', BASE.DIRECTORY_SEPARATOR.'data');
-  define('CACHE', BASE.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'cache');
-  define('DEFAULTS', BASE.DIRECTORY_SEPARATOR.'defaults');
-  define('MODULES', BASE.DIRECTORY_SEPARATOR.'modules');
+define('BASE', (($_SERVER['DOCUMENT_ROOT'] != '') ? $_SERVER['DOCUMENT_ROOT'] : dirname(realpath(__FILE__))));
+define('NAME', 'RetroPie');
+define('BRAND', 'core/img/site-logo.png');
+define('COOKIE_LIFETIME', 60*60*24*7*4*3);
 
-  define('MODULE', 'config.json');
-  define('URL_SEPARATOR', '/');
+define('INC', BASE.DIRECTORY_SEPARATOR.'core');
+define('JS', BASE.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'js');
+define('CSS', BASE.DIRECTORY_SEPARATOR.'core'.DIRECTORY_SEPARATOR.'css');
+define('DATA', BASE.DIRECTORY_SEPARATOR.'data');
+define('CACHE', BASE.DIRECTORY_SEPARATOR.'data'.DIRECTORY_SEPARATOR.'cache');
+define('DEFAULTS', BASE.DIRECTORY_SEPARATOR.'defaults');
+define('MODULES', BASE.DIRECTORY_SEPARATOR.'modules');
 
-  define('FILE_CACHE', false);
-  define('FILE_COMPRESS', false);
-  
-  config::includes(INC);
-  session::construct();
+define('MODULE', 'config.json');
+define('URL_SEPARATOR', '/');
+
+define('FILE_CACHE', false);
+define('FILE_COMPRESS', false);
+
+config::includes(INC);
+session::construct();
   
 class config 
 {
