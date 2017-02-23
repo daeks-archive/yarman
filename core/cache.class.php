0c467c0184370eaa1117dc4a872c8d39be072921
daeks <daeks@users.noreply.github.com>
2017-02-23 12:50:30 +0100

    Update cache.class.php

--- core/cache.class.php
+++ core/cache.class.php
@@ -1,17 +1,17 @@
 <?php
 
-class cache 
+class cache
 {
   public static function setClientVariable($key, $val, $time = COOKIE_LIFETIME)
   {
     $_SESSION[$key] = $val;
-    setcookie ($key, rawurlencode($val), time() + $time, URL_SEPARATOR);
+    setcookie($key, rawurlencode($val), time() + $time, URL_SEPARATOR);
   }
 
   public static function unsetClientVariable($key)
   {
     unset($_SESSION[$key]);
-    setcookie ($key, '', -1, URL_SEPARATOR);
+    setcookie($key, '', -1, URL_SEPARATOR);
   }
 
   public static function getClientVariable($key)
