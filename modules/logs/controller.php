feeef34d9f78ada063decc3d9f4ef7dc018d1e6f
daeks <daeks@gmx.net>
2017-02-28 12:14:42 +0100

    retropie-setup log support

--- modules/logs/controller.php
+++ modules/logs/controller.php
@@ -6,11 +6,24 @@ if (network::get('action') != '') {
   switch (network::get('action')) {
     case 'view':
       if (network::get('id') != '') {
+        $parts = explode('@', network::get('id'));
         $output = '';
         foreach (db::read($module->id) as $item) {
-          if ($item['id'] == network::get('id')) {
-            if (file_exists($item['value'])) {
-              $output = file_get_contents($item['value']);
+          if (sizeof($parts) == 2) {
+            if ($item['id'] == $parts[1]) {
+              if (file_exists($item['value'].DIRECTORY_SEPARATOR.$parts[0])) {
+                $file = gzopen($item['value'].DIRECTORY_SEPARATOR.$parts[0], 'r');
+                while ($line = gzgets($file, 1024)) {
+                  $output .= $line;
+                }
+                gzclose($file);
+              }
+            }
+          } else {
+            if ($item['id'] == network::get('id')) {
+              if (file_exists($item['value'])) {
+                $output = file_get_contents($item['value']);
+              }
             }
           }
         }
--- modules/logs/index.php
+++ modules/logs/index.php
@@ -11,11 +11,22 @@ echo '<div class="col-sm-12">';
 echo '<select name="nav-logs" id="nav-logs" class="form-control" data-mode="text" data-query="'.CONTROLLER.'?action=view&id=" data-target="module-content">';
 echo '<option value="" selected>-- Select Log File --</option>';
 foreach (db::read($module->id) as $item) {
-  echo '<option';
-  if (cache::getClientVariable($module->id.'_id') == $item['id']) {
-    echo ' selected';
+  if ($item['type'] == 'file') {
+    echo '<option';
+    if (cache::getClientVariable($module->id.'_id') == $item['id']) {
+      echo ' selected';
+    }
+    echo ' value="'.$item['id'].'">'.$item['value'].'</option>';
+  } elseif ($item['type'] == 'folder') {
+    foreach (array_slice(scandir($item['value']), 2) as $object) {
+      echo '<option';
+      $parts = explode('@', cache::getClientVariable($module->id.'_id'));
+      if (sizeof($parts) == 2 && $parts[0] == $object) {
+        echo ' selected';
+      }
+      echo ' value="'.$object.'@'.$item['id'].'">'.$item['value'].DIRECTORY_SEPARATOR.$object.'</option>';
+    }
   }
-  echo ' value="'.$item['id'].'">'.$item['value'].'</option>';
 }
 echo '</select><br>';
 echo '</div>';
