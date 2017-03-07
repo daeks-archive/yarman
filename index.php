68a956cb6e710d45543fdffe23caf27afce8e962
daeks <daeks@gmx.de>
2017-03-07 08:11:29 +0100

    switch to http_host

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ foreach ($modules as $moduleconfig) {
       $target = $item->panel;
       if (strpos($target, URL_SEPARATOR) !== 0) {
         $target = str_replace(BASE, '', MODULES.URL_SEPARATOR.$tmp->id.URL_SEPARATOR.$target);
-        $target = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'].$target;
+        $target = 'http://'.$_SERVER['HTTP_HOST'].$target;
       }
       
       $parts = explode(' ', $item->grid);
