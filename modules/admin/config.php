8353c62608214c61d7864ae1790c7a0eff385ba4
daeks <daeks@gmx.de>
2017-03-08 09:29:39 +0100

    format

--- modules/admin/config.php
+++ modules/admin/config.php
@@ -3,7 +3,7 @@
 require_once(dirname(realpath(__DIR__)).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config.php');
 $module = module::read();
 
-if ($module != null) { 
+if ($module != null) {
   config::includes(MODULES.DIRECTORY_SEPARATOR.$module->id);
 }
 
